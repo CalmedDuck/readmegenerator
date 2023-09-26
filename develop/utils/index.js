1ed6a65f1afb5d5b8fb2b0c60c950433373eca6b
Brock Williams <b.williams54255@gmail.com>
2023-09-25 23:23:44 -0700

    Made function to create readme.

--- develop/utils/index.js
+++ develop/utils/index.js
@@ -52,10 +52,62 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const licenseBadge = {
+        'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+        'Apache 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+        'GPL 3.0': '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+        'BSD 3': '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+        'None': ''
+    };
+
+    const READMEContent = `
+# ${data.title} ${licenseBadge[data.license]}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license !== 'None' ? `This project is covered under the ${data.license} license.` : 'This project is not licensed.'}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any additional questions, please reach out to me on:
+- [GitHub](https://github.com/${data.github})
+- Email: ${data.email}
+    `;
+
+    fs.writeFileSync(fileName, READMEContent, err => {
+        if (err) throw err;
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(data => {
+        writeToFile('README.md', data);
+        console.log('Successfully generated README.md');
+    });
+}
 
 // Function call to initialize app
 init();
